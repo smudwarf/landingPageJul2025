ff65b7b6eda0a95359ccea09d534f95ffbca2a0a
Sine Munch <smu@dwarf.dk>
2025-11-17 15:38:55 +0100

    feat: spinner background

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import IntroScene from "./IntroScene.vue";
-import SpinScene from "./SpinScene.vue";
-import Spinner from "./spinner.vue";
+import SpinScene from "./SpinScene.vue"; 
+import Circle from "./circle.vue";
 
-const introComplete = ref(false);
+const introComplete = ref(true);
 </script>
 <template>
-  <IntroScene @complete="introComplete = true" v-if="!introComplete" /> 
-  <SpinScene v-if="introComplete" />
+ <!-- <IntroScene @complete="introComplete = true" v-if="!introComplete" /> 
+  <SpinScene v-if="introComplete" />-->
+  <Circle v-if="introComplete" />
   <!--<spinner v-if="!introComplete" />-->
 </template>
 <style scoped></style>
---
+++ src/components/circle.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import gsap from "gsap";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  gsap.to("[data-top-spinner]", {
+    scale: 1.1,
+    duration: 2,
+    ease: "Power2.InOut",
+    yoyo: true,
+    repeat: -1,
+  });
+  gsap.to("[data-bottom-spinner]", {
+    scale: 1.1,
+    duration: 2,
+    ease: "Power2.InOut",
+    yoyo: true,
+    repeat: -1,
+  });
+});
+</script>
+
+<template>
+  <div class="relative h-screen w-screen">
+    <h1>hej</h1>
+   <div class="relative w-full max-w-[60rem] mx-auto aspect-square">
+    <!-- top -->
+    <img
+      src="public/images/top-spinner.svg"
+      data-top-spinner
+      class="absolute inset-0 m-auto origin-center translate-y-[-5px]"
+    />
+
+    <!-- bottom -->
+    <img
+      src="public/images/bottom-spinner.svg"
+      data-bottom-spinner
+      class="absolute inset-0 m-auto origin-center translate-y-[5px]"
+    />
+  </div>
+  </div>
+</template>
+
+<style scoped></style>
